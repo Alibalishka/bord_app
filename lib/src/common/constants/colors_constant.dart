4be45efcbee2c6b327f779f113cea6801f2bcd54
alibalishka <groznyi.alik@gmail.com>
2022-11-10 18:57:37 +0600

    add constants

---
+++ lib/src/common/constants/colors_constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color white = Color(0xFFFEFEFE);
+  static const Color black = Colors.black;
+  static const Color primary = Color(0xff0AAB39);
+  static const Color inActive = Color(0xFFC4C4C4);
+}
---
+++ lib/src/common/constants/padding_constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class AppPadings{
+  static const EdgeInsets horizontal = EdgeInsets.symmetric(horizontal: 40);
+  static const EdgeInsets primary = EdgeInsets.symmetric(horizontal: 19);
+  static const EdgeInsets vertical = EdgeInsets.symmetric(horizontal: 16);
+}
